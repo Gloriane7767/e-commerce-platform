6066d65dba4167bafea87037eecc57bed5719a56
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-02-18 13:26:21 +0100

    update ECommercePlatformApplicationwith class to runner

--- src/main/java/com/gloriane/ecommerceplatform/runner.java
+++ src/main/java/com/gloriane/ecommerceplatform/runner.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommercePlatformApplication {
+public class runner {
 
     public static void main(String[] args) {
-        SpringApplication.run(ECommercePlatformApplication.class, args);
+        SpringApplication.run(runner.class, args);
     }
 
 }
