66f4328023842815232657046d8bcc52d6819365
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-02-18 15:26:34 +0100

    add application.properties bundles

--- src/main/java/com/gloriane/ecommerceplatform/ECommercePlatformApplication.java
+++ src/main/java/com/gloriane/ecommerceplatform/ECommercePlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class runner {
+public class ECommercePlatformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(runner.class, args);
+        SpringApplication.run(ECommercePlatformApplication.class, args);
     }
 
 }
