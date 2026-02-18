14b097e2920c32bfcef7d5b4a869475d9a172823
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-02-18 13:22:22 +0100

    update Customer and Address classes with extends jpaRepository

--- src/main/java/com/gloriane/ecommerceplatform/repository/Address.java
+++ src/main/java/com/gloriane/ecommerceplatform/repository/Address.java
@@ -1,7 +1,8 @@
 package com.gloriane.ecommerceplatform.repository;
 
 import org.hibernate.annotations.processing.Find;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Address {
+public interface Address extends JpaRepository {
     void findAllAddressBySpecificZipCode(String zipCode);
 }
--- src/main/java/com/gloriane/ecommerceplatform/repository/Customer.java
+++ src/main/java/com/gloriane/ecommerceplatform/repository/Customer.java
@@ -1,6 +1,8 @@
 package com.gloriane.ecommerceplatform.repository;
 
-public interface Customer {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Customer extends JpaRepository {
      void findCustomerByUniqueEmail(String email);
     void findCustomersByLastName(String lastName);
      void findCustomersBySpecificCity(String city);
