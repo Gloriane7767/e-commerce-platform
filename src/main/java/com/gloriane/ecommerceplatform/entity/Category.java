15c23e052cec7dc9327ff9ccb1f706fc2bda4867
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-02-24 23:08:55 +0100

    add Category.java, map Product.java to Category.java

---
+++ src/main/java/com/gloriane/ecommerceplatform/entity/Category.java
@@ -0,0 +1,28 @@
+package com.gloriane.ecommerceplatform.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
--- src/main/java/com/gloriane/ecommerceplatform/entity/Product.java
+++ src/main/java/com/gloriane/ecommerceplatform/entity/Product.java
@@ -4,8 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -15,7 +15,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "products")
-class Product {
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,12 +23,25 @@ class Product {
     @Column(nullable = false)
     private String name;
 
-    @Column(name = "image_url")
-    private List<String> imageUrls;
+    @ElementCollection // Store the elements of this collection in a table named product_images.
+    @CollectionTable( // The join column product_id links each image row to the owning product row (the product's primary key).
+            name = "product_images",
+            joinColumns = @JoinColumn(name = "product_id")
+    )
+    @Column(name = "image_url") // The column that stores the String values in the product_images table will be named image_url.
+    private List<String> imageUrls = new ArrayList<>();
 
     @Column(nullable = false)
     private BigDecimal price;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 }
+
+ /*
+    You have a field in a JPA entity: List<string> imageUrls;</string>
+    The annotations tell JPA to store that list in its own database table (not as columns of the main product table) even though the list items are plain Strings (not separate entity objects).
+    This field is a collection of basic values (Strings, numbers, enums) or embeddables, not a collection of other entity objects."
+    JPA will treat each String in the list as a value to be persisted in a separate collection table that belongs to the parent entity.
+     */
