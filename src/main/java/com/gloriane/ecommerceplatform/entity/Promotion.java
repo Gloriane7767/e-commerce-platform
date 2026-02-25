9c1016e8fcdcb1e660b9dfc14f493126496999f6
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-02-25 10:04:47 +0100

    add Promotion.java and map them

---
+++ src/main/java/com/gloriane/ecommerceplatform/entity/Promotion.java
@@ -0,0 +1,32 @@
+package com.gloriane.ecommerceplatform.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "promotions")
+public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @ManyToMany(mappedBy = "promotions")
+    private Set<Product> products;
+}
